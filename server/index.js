ec3ed5c054577229f06e7bc93ca9f93141aebd22
johnmuthua <caveomuthama@gmail.com>
2020-04-15 13:22:47 -0700

    added dev file to git ignore

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://localhost:27017/todoapp",
-  { useNewUrlParser: true }
-    .then(() => console.log("connected"))
-    .catch((err) => console.log(err))
-);
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.get("/", (req, res) => {
-  res.send("hellow world");
+const { User } = require("./models/user");
+const config = require("../config/key.js");
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("connected"))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000);
