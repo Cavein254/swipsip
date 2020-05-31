e37289a87778adc569f41713615e561fa931483b
johnmuthua <caveomuthama@gmail.com>
2020-05-31 10:42:20 +0300

    removed unused packages in client pacckage.json

--- client/src/components/admin/AddProduct.js
+++ client/src/components/admin/AddProduct.js
@@ -6,23 +6,7 @@ const AddProduct = () => {
     <div>
       <Form>
         <h2 ClassName="formheader">Add Product</h2>
-        <div className="mb-3">
-          <Form.File id="formcheck-api-custom" custom>
-            <Form.File.Input isValid />
-            <Form.File.Label data-browse="Upload">
-              Choose Picture
-            </Form.File.Label>
-            <Form.Control.Feedback type="valid">
-              You did it!
-            </Form.Control.Feedback>
-          </Form.File>
-        </div>
-        <div className="mb-3">
-          <Form.File id="formcheck-api-regular">
-            <Form.File.Label>Regular file input</Form.File.Label>
-            <Form.File.Input />
-          </Form.File>
-        </div>
+
         <Form.Group controlId="formBasicText">
           <Form.Label>Name</Form.Label>
           <Form.Control type="text" placeholder="Product Name" name="title" />
